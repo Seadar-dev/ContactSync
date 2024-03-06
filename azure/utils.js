ad350bab38ff40b7d2aa3d55da107ab2e135e063
Dani Abouhamad <abouhamad.d@husky.neu.edu>
2024-03-06 11:00:04 -0500

    debugging renew

--- azure/utils.js
+++ azure/utils.js
@@ -1,6 +1,6 @@
 export function expirationDate() {
   let expirationDate = new Date();
-  // expirationDate.setDate(expirationDate.getDate() + 6);
-  expirationDate.setMinutes(expirationDate.getMinutes() + 3);
+  expirationDate.setDate(expirationDate.getDate() + 6);
+  // expirationDate.setMinutes(expirationDate.getMinutes() + 3);
   return expirationDate;
 }
