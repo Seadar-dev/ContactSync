7e80eebf37617fc0426a48c6e47483160da13925
Dani Abouhamad <abouhamad.d@husky.neu.edu>
2024-03-07 15:18:12 -0500

    building update

--- azure/fixes/undoEdit.js
+++ azure/fixes/undoEdit.js
@@ -6,10 +6,13 @@ export default async function undoEdit(body) {
   const client = await auth();
 
   const isValid = await validateEdit(client, body);
-
-
-
   console.log("Is Valid: ", isValid)
+
+
+  const contact = await masterContact(client, masterContactId);
+  const res = await client.api(`${process.env.DIRECTORY_PATH}/${body.id}`).patch({ ...contact, spouseName: body.spouseName });
+  console.log(res);
+
 
   console.log("Undoing Edit")
   return;
@@ -30,24 +33,17 @@ async function validateEdit(client, body) {
 
     EQUALITY_FIELDS.forEach((field) => {
       isSame = isSame && contact[field] === directoryCont[field];
-      console.log(field, isSame);
     })
-
 
     isSame = isSame && JSON.stringify(contact.emailAddresses) === JSON.stringify(directoryCont.emailAddresses);
 
-    console.log("2", JSON.stringify(contact.emailAddresses), JSON.stringify(directoryCont.emailAddresses))
     isSame = isSame && JSON.stringify(contact.businessPhones) === JSON.stringify(directoryCont.businessPhones);
-
-    console.log("3", JSON.stringify(contact.businessPhones) === JSON.stringify(directoryCont.businessPhones))
 
     return isSame;
 
-    // console.log("Directory Contact: ", body);
-    // console.log("Assosciated Master Contact: ", contact);
   } catch (err) {
     console.log(err);
-    return false
+    return true;
   }
 }
 
