fbe4b9c5e19eeedb99851a40853f0a93ca246ba7
Dani Abouhamad <abouhamad.d@husky.neu.edu>
2024-03-08 15:47:25 -0500

    master create

--- masterSync/masterCreate.js
+++ masterSync/masterCreate.js
@@ -1,6 +1,20 @@
-export default function masterCreate() {
+import auth from "../azure/auth";
+
+export default async function masterCreate(body, logChange) {
   console.log("Master Create");
+
+  const client = await auth();
+
+  const newContact = await client.api(process.env.DIRECTORY_PATH).post({ ...body, spouseName: body.id });
+
+  logChange(newContact.changeKey);
+
+  const res = await client.api(`${process.env.MASTER_PATH}/${body.id}`).patch({ spouseName: newContact.id });
 
   return;
 
 }
+
+const EQUALITY_FIELDS = [
+  "birthday", "generation", "givenName", "title", "surname", "jobTitle"
+]
--- masterSync/masterEdit.js
+++ masterSync/masterEdit.js
@@ -1,5 +1,6 @@
-export default function masterEdit() {
+export default async function masterEdit(body, logChange) {
   console.log("Master Edit");
+  console.log(body);
 
   //Don't make propogate update if change is to the spouseName
 }
