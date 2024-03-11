232da901713f48b488b821641f15f4d65271ea10
Dani Abouhamad <abouhamad.d@husky.neu.edu>
2024-03-11 10:23:58 -0400

    master create

--- masterSync/masterCreate.js
+++ masterSync/masterCreate.js
@@ -1,13 +1,20 @@
 import auth from "../azure/auth.js";
+import { masterContact } from "../utils.js";
 
 export default async function masterCreate(body, logChange) {
   console.log("Master Create");
 
   const client = await auth();
 
-  console.log(JSON.stringify({ ...body, spouseName: body.id }))
+  const contact = await masterContact(client, body.id);
+  let temp = {};
+  [...SUBBED_ARRAY_FIELDS, ...SUBBED_STRING_FIELDS].forEach(field => temp[field] = contact[field]);
 
-  const newContact = await client.api(process.env.DIRECTORY_PATH).post(JSON.stringify({ ...body, spouseName: body.id }));
+  const newContact = await client.api(process.env.DIRECTORY_PATH).post({ ...temp, spouseName: body.id });
+
+  // console.log(JSON.stringify({ ...body, spouseName: body.id }))
+
+  // const newContact = await client.api(process.env.DIRECTORY_PATH).post(JSON.stringify({ ...body, spouseName: body.id }));
 
   logChange(newContact.changeKey);
 
