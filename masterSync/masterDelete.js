fb57fb17b1305dc241b8b83d83766dcc2991a3cf
Dani Abouhamad <abouhamad.d@husky.neu.edu>
2024-03-11 11:32:14 -0400

    master create

--- masterSync/masterDelete.js
+++ masterSync/masterDelete.js
@@ -1,5 +1,8 @@
+import auth from "../azure/auth.js";
+
 export default async function masterDelete(body) {
   console.log("Master Delete");
+  const client = await auth();
 
   const contactToDelete = (await client.api(`${process.env.MASTER_PATH}`).get()).value.find(directory => directory.spouseName === body.id);
 
