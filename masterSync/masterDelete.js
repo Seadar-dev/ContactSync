83a4dbcf1ab0e38c18e03b0eb36b3a2708b04801
Dani Abouhamad <abouhamad.d@husky.neu.edu>
2024-03-11 11:35:25 -0400

    master delete

--- masterSync/masterDelete.js
+++ masterSync/masterDelete.js
@@ -6,6 +6,8 @@ export default async function masterDelete(body) {
 
   const contactToDelete = (await client.api(`${process.env.MASTER_PATH}`).get()).value.find(directory => directory.spouseName === body.id);
 
+  if (!contactToDelete) return;
+
   const res = await client.api(`${process.env.DIRECTORY_PATH}/${contactToDelete.id}`).delete();
   console.log(res)
 }
