2050b94f6571319baa1e170555498b375099366b
Dani Abouhamad <abouhamad.d@husky.neu.edu>
2024-03-12 14:44:52 -0400

    updated get subs to all be relevant ones

--- routes/subscriptions.js
+++ routes/subscriptions.js
@@ -5,7 +5,13 @@ const router = express.Router()
 //Gets a list of all active subscriptions
 router.get('/', async (req, res) => {
   console.log("Getting subscriptions");
-  const allSubscriptions = await subscriptions();
+  let allSubscriptions;
+  try {
+    allSubscriptions = await subscriptions();
+
+  } catch (err) {
+    console.log(err);
+  }
   res.status(200).send(allSubscriptions)
 })
 export default router;
